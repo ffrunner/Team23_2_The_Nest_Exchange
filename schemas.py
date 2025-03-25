7a7d6cce64f8f69b01b47ea67b8cc9176e38fb47
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-25 18:47:29 -0400

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+#Pydantic Models used for data management (requests and responses) 
+
+#Data to get from user when signing up must match these data types/structure. Ex: Won't allow an int for a name
+class CreateUser(BaseModel):
+    id: int 
+    email: str
+    username: str 
+    password_hash: str
+    role: str
+    first_name: str
+    last_name: str
+    phone: str
+
+   
+
+
