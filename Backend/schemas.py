0f0c05a948cc44d99623af83b7c72cac9d87f7ee
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-04-02 19:46:37 -0400

    Update schemas.py

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from typing import List, Optional 
 
 #Pydantic Models used for data management (requests and responses) 
 
@@ -23,6 +23,29 @@ class ChangePassword(BaseModel):
     email: str 
     current_password: str 
     new_password: str  
+
+
+class ItemCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+    category_id: Optional[int] = None
+    lister_id: int
+
+class ItemUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    category_id: Optional[int] = None
+
+class ClaimCreate(BaseModel):
+    lister_id: int
+    claimer_id: int
+    pickup_details: Optional[str] = None
+    claim_status: str
+
+class ListingPhotoCreate(BaseModel):
+    item_id: int
+    photo_url: str
+
 
    
 
