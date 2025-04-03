a4ea3d18223976f32c7026bad892d555a29613af
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-04-03 13:23:49 -0400

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 
 #can add more origins. We'll need to add the actual link to nestexchange
 origins = [
-    "http://localhost:8000"
+    "http://localhost:8000",
+    "http://localhost:5173/"
 ]
 app.add_middleware(
     CORSMiddleware, 
