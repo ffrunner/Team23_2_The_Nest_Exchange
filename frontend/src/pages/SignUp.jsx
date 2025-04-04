c987239045e3f19972c1b19a2826102b86684504
ffrunner <100149981+ffrunner@users.noreply.github.com>
2025-04-04 09:13:43 -0400

    axios update

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 
 import '../css/SignUp.css'; // Assuming a separate CSS file
 
 const SignUp = () => {
