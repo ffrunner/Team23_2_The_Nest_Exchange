90fd1f7f82fd506e02a3e3969d2b8bdeae8fda9b
ffrunner <100149981+ffrunner@users.noreply.github.com>
2025-04-09 14:38:29 -0400

    forgotPW update

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../css/ForgotPassword.css'; 
+import '../css/ForgotPassword.module.css'; 
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('');
