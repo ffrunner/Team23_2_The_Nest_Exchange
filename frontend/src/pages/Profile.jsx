1f8a93e5a4970377f348746adc0a2657f4888d56
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-04-17 13:49:23 -0400

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -133,8 +133,7 @@ const fetchItems = async () => {
                                ))
                             )}
                         </div>
-         
-            )}
+         )}
                     
                     {selectedSection === "Listings" && (
                         <div className="listings-container">
